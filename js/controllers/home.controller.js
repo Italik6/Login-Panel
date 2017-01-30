254a9116b6202e499e558032f7ea3a208bb13c57
Italik6 <italik@tlen.pl>
2017-01-30 21:41:30 +0100

    init

---
+++ js/controllers/home.controller.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['UserService', '$rootScope', '$scope'];
+    function HomeController(UserService, $rootScope, $scope) {
+       $scope.no_data = 'N/A';
+        var vm = this;
+
+        vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+
+        initController();
+
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+  
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+        }
+        
+    }
+    
+})();
---
+++ js/controllers/login.controller.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService', '$scope'];
+
+    function LoginController($location, AuthenticationService, FlashService, $scope) {
+        var vm = this;
+        
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+
+    }
+
+})();
